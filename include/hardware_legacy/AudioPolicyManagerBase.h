e393a4738c6cf97d91fe8532c20f32a30d9efa26
Eric Laurent <elaurent@google.com>
2013-10-12 16:29:39 -0700

    audio policy: fix gapless playback when effects enabled
    Do not allow offloading if one non offloadable effect is enabled. This prevents from
    creating an offloaded track and tearing it down immediately after start when audioflinger
    detects there is an active non offloadable effect.
    Not doing so breaks gapless playback because AwesomePlayer will always be able to open
    an offloaded track which will not be compatible with the previous PCM track and
    Audiosink::open() will not recycle it.
    
    Bug: 11181933.
    Change-Id: I6d163806849d9093b9ec701ba8e8c1ccb658e3c8

--- include/hardware_legacy/AudioPolicyManagerBase.h
+++ include/hardware_legacy/AudioPolicyManagerBase.h
@@ -493,6 +493,8 @@ protected:
 
         audio_io_handle_t selectOutputForEffects(const SortedVector<audio_io_handle_t>& outputs);
 
+        bool isNonOffloadableEffectEnabled();
+
         //
         // Audio policy configuration file parsing (audio_policy.conf)
         //
